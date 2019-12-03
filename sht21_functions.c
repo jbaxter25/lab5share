b0ba7856c2d5676fe7b0ea62bf20b0b0f1ba41ef
Jordan Baxter <baxterjo@oregonstate.edu>
2019-12-03 10:21:18 -0800

    Initial commit.

---
+++ sht21_functions.c
@@ -0,0 +1,32 @@
+// lm73_functions.c       
+// Jordan Baxter
+
+#include <util/twi.h>
+#include "sht21_functions.h"
+#include <util/delay.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+//********************************************************************************
+//                              sht21_temp_convert
+//
+//given a temperature reading from an sht21, the address of a buffer
+//array, and a format (deg F or C) it formats the temperature into ascii in 
+//the buffer pointed to by the arguement.
+//******************************************************************************
+void sht21_temp_convert(char* to_str, uint16_t sense_temp, uint8_t f_not_c){
+    //sense_temp &= ~3;                                 //Set two LSB to zero (per datasheet) before starting temp calculation
+    float temp = (((float)sense_temp/65536)*175.72)-46.85;   //Convert read value to temperature using formula in datasheet
+    if(f_not_c){
+        //itoa((int)temp, temp_digits, 10); 
+        dtostrf(temp, 5, 1, to_str);
+    } else {
+        temp = temp * 1.8 + 32;
+        //itoa((int)temp, temp_digits, 10);
+        dtostrf(temp, 5, 1, to_str);
+    }
+
+}//sht21_temp_convert
+//******************************************************************************
---
+++ sht21_functions.h
@@ -0,0 +1,24 @@
+// sht21_functions.h 
+// Jordan Baxter 11.28.19
+//special defines and functions for the sht21 humidity and temperature sensor
+//This header file and the matching .c file were started from LM73 functions files
+//written by Roger Traylor.
+
+#define F_CPU 16000000UL
+#define TRUE  1
+#define FALSE 0
+
+#include "twi_master.h"  //my TWCR_START, STOP, NACK, RNACK, SEND
+//use the status codes in: usr/local/AVRMacPack/avr-3/include/util/twi.h
+
+#define SHT21_ADDRESS 0x80                      //SHT21, address pin floating
+#define SHT21_WRITE (SHT21_ADDRESS | TW_WRITE)  //LSB is a zero to write
+#define SHT21_READ  (SHT21_ADDRESS | TW_READ)   //LSB is a one to read
+#define SHT21_TEMP_HOLD         0xE3            //SHT21 temperature command, hold clk low while processing
+#define SHT21_RH_HOLD           0xE5            //SHT21 relative humidity command, hold clk low while processing
+#define SHT21_TEMP_NO_HOLD      0xF3            //SHT21 temp command, do not hold clk low while processing, requires master to poll
+#define SHT21_RH_NO_HOLD        0xF5            //SHT21 relative humidity command, do not hold clk low while processing, requires master to poll
+
+//special functions for lm73 temperature sensor
+void sht21_temp_convert(char* to_str, uint16_t sense_temp, uint8_t f_not_c);
+  
